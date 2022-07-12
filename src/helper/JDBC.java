0fcc00a1d1532681b04de2ccb69e7b2d981b2afc
VivyenVu <pokeviv@gmail.com>
2022-07-12 15:51:47 -0400

    try catch blocks for connecting to database and closing connection

--- src/helper/JDBC.java
+++ src/helper/JDBC.java
@@ -1,6 +1,7 @@
 package helper;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public abstract class JDBC {
     private static final String protocol = "jdbc";
@@ -13,4 +14,27 @@ public abstract class JDBC {
     private static final String userName = "sqlUser";
     private static final String password = "Passw0rd!";
     public static Connection connection;
+
+    //connecting to database
+    public static void openConnection() {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(jdbUrl, userName, password);
+            System.out.println("Connection successful");
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    //closing that connection
+    public static void closeConnection() {
+        try{
+            connection.close();
+            System.out.println("Connection closed");
+        }
+        catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
