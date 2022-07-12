69cdb807ab8ae18074671a6931da7ab1d566da25
VivyenVu <pokeviv@gmail.com>
2022-07-12 11:11:10 -0400

    Main.java test french

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Locale.setDefault(new Locale("fr"));
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
