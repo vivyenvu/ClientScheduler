1c5818f615eff93d9103e70096f92ce474c17cf6
VivyenVu <pokeviv@gmail.com>
2022-07-12 15:46:22 -0400

    JDBC driver, un, pass, connetion variables

--- src/helper/JDBC.java
+++ src/helper/JDBC.java
@@ -1,9 +1,16 @@
 package helper;
 
+import java.sql.Connection;
+
 public abstract class JDBC {
     private static final String protocol = "jdbc";
     private static final String vendor = ":mysql:";
     private static final String location = "//localhost/";
     private static final String databaseName = "client_schedule";
-    private static final String jdbUrl = protocol + vendor + location + databaseName;
+    //helps with timezone conversion
+    private static final String jdbUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String userName = "sqlUser";
+    private static final String password = "Passw0rd!";
+    public static Connection connection;
 }
