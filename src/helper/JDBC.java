3f133a8095e97b57bda0962e7b31e7651b84b8f5
VivyenVu <pokeviv@gmail.com>
2022-07-12 15:42:27 -0400

    JDBS

---
+++ src/helper/JDBC.java
@@ -0,0 +1,9 @@
+package helper;
+
+public abstract class JDBC {
+    private static final String protocol = "jdbc";
+    private static final String vendor = ":mysql:";
+    private static final String location = "//localhost/";
+    private static final String databaseName = "client_schedule";
+    private static final String jdbUrl = protocol + vendor + location + databaseName;
+}
