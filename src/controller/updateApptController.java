356e8a8187cf555f8065d28ea4db2d3e00b734c5
VivyenVu <pokeviv@gmail.com>
2022-07-13 16:51:54 -0400

    add/update customer/appt cancel buttons to return to mainMenu

--- src/controller/updateApptController.java
+++ src/controller/updateApptController.java
@@ -1,11 +1,24 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class updateApptController {
     public void onUpdateApptSaveBtn(ActionEvent actionEvent) {
     }
 
-    public void onUpdateApptCancelBtn(ActionEvent actionEvent) {
+    public void onUpdateApptCancelBtn(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/mainMenu.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 400);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
