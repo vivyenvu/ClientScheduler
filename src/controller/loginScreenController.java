ff548859eb8624c304f79f2a30e38eb83c0ab9cf
VivyenVu <pokeviv@gmail.com>
2022-07-13 16:37:32 -0400

    Submit button from loginScreen.fxml connects to customerMainMenu.fxml

--- src/controller/loginScreenController.java
+++ src/controller/loginScreenController.java
@@ -1,10 +1,22 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class loginScreenController {
-    public void onClickSubmit(ActionEvent actionEvent) {
+    public void onClickSubmit(ActionEvent actionEvent) throws IOException {
         //add validation for username and password before going to Main Menu
-
+        Parent root = FXMLLoader.load(getClass().getResource("/view/customerMainMenu.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 400);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
