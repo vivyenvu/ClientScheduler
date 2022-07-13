5f3dc76d8f3f23366a5c7b57da3452db025236df
VivyenVu <pokeviv@gmail.com>
2022-07-13 16:49:13 -0400

    onApptAddBtn and onCustomerAddBtn scene change

--- src/controller/loginScreenController.java
+++ src/controller/loginScreenController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class loginScreenController {
     public void onClickSubmit(ActionEvent actionEvent) throws IOException {
         //add validation for username and password before going to Main Menu
-        Parent root = FXMLLoader.load(getClass().getResource("/view/customerMainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/mainMenu.fxml"));
         Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 1080, 400);
         stage.setTitle("Main Menu");
--- src/controller/mainMenuController.java
+++ src/controller/mainMenuController.java
@@ -1,15 +1,22 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class mainMenuController {
-    public void onWeekViewBtn(ActionEvent actionEvent) {
-    }
-
-    public void onMonthViewBtn(ActionEvent actionEvent) {
-    }
-
-    public void onCustomerAddBtn(ActionEvent actionEvent) {
+    public void onCustomerAddBtn(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/addCustomer.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Add Customer");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void onCustomerUpdateBtn(ActionEvent actionEvent) {
@@ -18,12 +25,24 @@ public class mainMenuController {
     public void onCustomerDeleteBtn(ActionEvent actionEvent) {
     }
 
-    public void onApptAddBtn(ActionEvent actionEvent) {
+    public void onApptAddBtn(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/addAppt.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Add Appointment");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void onApptUpdateBtn(ActionEvent actionEvent) {
     }
 
     public void onApptDeleteBtn(ActionEvent actionEvent) {
+    }
+
+    public void onWeekViewBtn(ActionEvent actionEvent) {
+    }
+
+    public void onMonthViewBtn(ActionEvent actionEvent) {
     }
 }
