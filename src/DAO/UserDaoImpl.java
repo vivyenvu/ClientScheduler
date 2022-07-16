c7479b126791cb7686d43ca1092e724593e87c01
VivyenVu <pokeviv@gmail.com>
2022-07-16 10:20:05 -0400

    sample userDaoImpl

--- src/DAO/UserDaoImpl.java
+++ src/DAO/UserDaoImpl.java
@@ -1,4 +1,21 @@
 package DAO;
 
 public class UserDaoImpl {
+    //example from first DAO webinar
+    public static User getUser(String userName) throws SQLException, Exception {
+        DBConnection.openConnection();
+        String sqlStatement = "select * FROM users WHERE User_Name = ' " + userName + "'";
+
+        Query.makeQuery(sqlStatement);
+        User userResult;
+        ResultSet result= Query.getResult();
+        while(result.next()) {
+            int userid = result.getInt("User_ID_");
+            String userNameG = result.getString("User_Name");
+            String password = result.getString("Password");
+            userResult = new User(userid, userName, password);
+            return userResult;
+            }
+
+    }
 }
