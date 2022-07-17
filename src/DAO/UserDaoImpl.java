b1e7adaa2b3aab2b38187b1eccb56e6425f9d997
VivyenVu <pokeviv@gmail.com>
2022-07-17 16:46:48 -0400

    my own getUser in UserDaoImpl

--- src/DAO/UserDaoImpl.java
+++ src/DAO/UserDaoImpl.java
@@ -4,27 +4,29 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Users;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDaoImpl {
-    //example from first DAO webinar
     public static Users getUser(String userName) throws SQLException, Exception {
-        DBConnection.openConnection();
-        String sqlStatement = "select * FROM users WHERE User_Name = ' " + userName + "'";
-
-        Query.makeQuery(sqlStatement);
         Users userResult;
-        ResultSet result= Query.getResult();
-        while(result.next()) {
-            int userid = result.getInt("User_ID_");
-            String userNameG = result.getString("User_Name");
-            String password = result.getString("Password");
-            userResult = new Users(userid, userName, password);
-            return userResult;
+        try{
+            String sql = "SELECT * FROM users WHERE User_Name = ' " + userName + "'";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                int userid = rs.getInt("User_ID_");
+                String username = rs.getString("User_Name");
+                String password = rs.getString("Password");
+                userResult = new Users(userid, username, password);
             }
-        DBConnection.closeConnection();
-        return null;
+        }
+        catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        return userResult;
     }
     public static ObservableList<Users> getAllUsers() throws SQLException, Exception{
         ObservableList<Users> allUsers = FXCollections.observableArrayList();
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,6 @@
 package sample;
 
 import DAO.DBConnection;
-import helper.JDBC;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
