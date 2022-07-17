500fda3ca7dd50703a016364ea18c7af1dc0ded4
VivyenVu <pokeviv@gmail.com>
2022-07-17 15:46:56 -0400

    Users constructor, getters, and setters

--- src/model/Users.java
+++ src/model/Users.java
@@ -10,4 +10,46 @@ public class Users {
         this.username = username;
         this.password = password;
     }
+
+    /**
+     * @return the userID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param id the ID set for the user
+     */
+    public void setUserID(int id){
+        userID = id;
+    }
+
+    /**
+     * @param name the username to set
+     */
+    public void setUsername(String name) {
+        username = name;
+    }
+
+    /**
+     * @param pw to set for the password
+     */
+    public void setPassword (String pw) {
+        password = pw;
+    }
 }
