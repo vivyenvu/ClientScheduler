1a7acf11a72983ebdd0cd803f04e9e8abb90cd6d
VivyenVu <pokeviv@gmail.com>
2022-07-17 15:42:00 -0400

    users class variables

--- src/model/Users.java
+++ src/model/Users.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Users {
+    private int userID;
+    private String username;
+    private String password;
+
+    public Users(int userID, String username, String password){
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
 }
