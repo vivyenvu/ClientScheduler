e4d4d55235e99df3b7a9e2c13ec8ba7332124410
VivyenVu <pokeviv@gmail.com>
2022-07-19 10:48:08 -0400

    Contacts variables and constructor

--- src/helper/Contacts.java
+++ src/helper/Contacts.java
@@ -1,4 +1,13 @@
 package helper;
 
 public class Contacts {
+    private int contact_ID;
+    private String contact_name;
+    private String email;
+
+    public Contacts (int contact_ID, String contact_name, String email){
+        this.contact_ID = contact_ID;
+        this.contact_name = contact_name;
+        this.email = email;
+    }
 }
