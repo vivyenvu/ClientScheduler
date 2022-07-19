668d671e92c7e27a11f347a2b42a763c67566a92
VivyenVu <pokeviv@gmail.com>
2022-07-19 10:53:12 -0400

    FirstClassDivision class variables and constructor

--- src/helper/FirstClassDivisions.java
+++ src/helper/FirstClassDivisions.java
@@ -1,4 +1,13 @@
 package helper;
 
 public class FirstClassDivisions {
+    private int divisionID;
+    private String division;
+    private int countryID;
+
+    public FirstClassDivisions (int divisionID, String division, int countryID) {
+        this.divisionID = divisionID;
+        this.division = division;
+        this.countryID = countryID;
+    }
 }
