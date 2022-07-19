82412c7d22222a2ec93d64bbcbc5f716e840c510
VivyenVu <pokeviv@gmail.com>
2022-07-19 12:01:26 -0400

    getters and setters for Contacts

--- src/model/Contacts.java
+++ src/model/Contacts.java
@@ -10,4 +10,28 @@ public class Contacts {
         this.contactName = contactName;
         this.email = email;
     }
+
+    public int getContactID(){
+        return contactID;
+    }
+
+    public String getContactName(){
+        return contactName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setContactID(int id) {
+        contactID = id;
+    }
+
+    public void setContactName(String name) {
+        contactName = name;
+    }
+
+    public void setEmail(String emailAddress) {
+        email = emailAddress;
+    }
 }
