f55c82b19bf446693f66fc4c71ddae1ee94ccd40
VivyenVu <pokeviv@gmail.com>
2022-07-22 10:47:26 -0400

    properly loads Customer Table data

--- src/DAO/CustomerDaoImpl.java
+++ src/DAO/CustomerDaoImpl.java
@@ -11,14 +11,14 @@ import java.sql.SQLException;
 public class CustomerDaoImpl {
     public static ObservableList<Customers> getAllCustomers() throws SQLException {
         ObservableList<Customers> allCustomers = FXCollections.observableArrayList();
-        ResultSet result = Query.getRS("select * from users");
+        ResultSet result = Query.getRS("select * from customers");
         while(result.next()){
             int custID = result.getInt("Customer_ID");
             String custName = result.getString("Customer_Name");
             String custAddress = result.getString("Address");
             String custPostal = result.getString("Postal_Code");
             String custPhone = result.getString("Phone");
-            int custDivID = result.getInt("Divison_ID");
+            int custDivID = result.getInt("Division_ID");
             Customers custResult = new Customers(custID, custName, custAddress, custPostal, custPhone, custDivID);
             allCustomers.add(custResult);
         }
