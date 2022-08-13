be63caf73e0c22af6cd5ca958163b1be6822d2ca
VivyenVu <pokeviv@gmail.com>
2022-08-13 09:37:30 -0400

    set up reportsSchedule scene and table

--- src/controller/reportsScheduleController.java
+++ src/controller/reportsScheduleController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class reportsScheduleController {
+    public void onCancelReportsBtn(ActionEvent actionEvent) {
+    }
+
+    public void onApptTypeBtn(ActionEvent actionEvent) {
+    }
+
+    public void onContactScheduleBtn(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerPerCountryBtn(ActionEvent actionEvent) {
+    }
 }
