b3fed5803a89492e78f989736d376583ef4defb3
unknown <pokeviv@gmail.com>
2022-09-03 12:03:51 -0400

    ApptDaoImpl javadocs

--- src/DAO/ApptDaoImpl.java
+++ src/DAO/ApptDaoImpl.java
@@ -10,7 +10,15 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+/**
+ * Pulls Appointments information from the database.
+ */
 public class ApptDaoImpl {
+    /**
+     * Populates a list of all appointments and their information from the database.
+     * @return list of all appointments
+     * @throws SQLException
+     */
     public static ObservableList<Appointments> getAllAppts() throws SQLException {
         ObservableList<Appointments> allAppts = FXCollections.observableArrayList();
         ResultSet result = Query.getRS("select * from appointments " +
@@ -40,8 +48,21 @@ public class ApptDaoImpl {
         return allAppts;
     }
 
+    /**
+     * Creates a new entry in the appointments table of the database.
+     * @param title appointment title
+     * @param desc appointment description
+     * @param loc appointment location
+     * @param type appointment type
+     * @param start appointment start time and date
+     * @param end appointment end time and date
+     * @param customerIDFK appointment associated customer id
+     * @param userIDFK appointment associated user id
+     * @param contactIDFK appointment associated contact id
+     * @throws SQLException
+     */
     public static void addAppt(String title, String desc, String loc, String type, LocalDateTime start, LocalDateTime end, int customerIDFK, int userIDFK, int contactIDFK) throws SQLException {
-        int apptID=0; //might not be a good initialization
+        int apptID=0;
         String sql = "INSERT INTO appointments VALUES (?,?,?,?,?,?,?,NULL, NULL, NULL, NULL,?,?,?)";
         PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
@@ -62,6 +83,11 @@ public class ApptDaoImpl {
         ps.execute();
     }
 
+    /**
+     * Deletes a row from the appointments table of the database.
+     * @param id id of the appointment to be deleted
+     * @throws SQLException
+     */
     public static void deleteAppt(int id) throws SQLException {
         //delete from appointments THEN delete from customer table
         try{
@@ -74,6 +100,19 @@ public class ApptDaoImpl {
         }
     }
 
+    /**
+     * Updates information of existing entry in the appointments table of database.
+     * @param apptID appointment id
+     * @param title appointment title
+     * @param desc appointment description
+     * @param loc appointment location
+     * @param type appointment type
+     * @param start appointment start time and date
+     * @param end appointment time and date
+     * @param customerIDFK appointment customer id
+     * @param userIDFK appointment user id
+     * @param contactIDFK appointment contact id
+     */
     public static void updateAppt (int apptID, String title, String desc, String loc, String type, LocalDateTime start, LocalDateTime end, int customerIDFK, int userIDFK, int contactIDFK){
         try{
             PreparedStatement ps = Query.getPS("UPDATE appointments set Title = ?, Description = ?, Location = ?, Type = ?, Start = ?, End = ?, Customer_ID = ?, User_ID = ?, Contact_ID = ? WHERE Appointment_ID = ?");
@@ -93,6 +132,12 @@ public class ApptDaoImpl {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Gets list of appointments that fall within the upcoming week.
+     * @return list of appointments that fall within the upcoming week.
+     * @throws SQLException
+     */
     public static ObservableList<Appointments> getWeekAppts() throws SQLException {
         ObservableList<Appointments> weekAppts = FXCollections.observableArrayList();
         LocalDateTime today = LocalDateTime.now();
@@ -126,6 +171,12 @@ public class ApptDaoImpl {
         }
         return weekAppts;
     }
+
+    /**
+     * Gets list of appointments that fall within the upcoming month.
+     * @return list of appointments that fall within the upcoming month.
+     * @throws SQLException
+     */
     public static ObservableList<Appointments> getMonthAppts() throws SQLException {
         ObservableList<Appointments> monthAppts = FXCollections.observableArrayList();
         LocalDateTime today = LocalDateTime.now();
@@ -159,6 +210,12 @@ public class ApptDaoImpl {
         }
         return monthAppts;
     }
+
+    /**
+     * Gets a list of unique types of appointments.
+     * @return a list of unique types of appointments.
+     * @throws SQLException
+     */
     public static ObservableList<String> getAllTypes() throws SQLException {
         ObservableList<String> allTypes = FXCollections.observableArrayList();
         ResultSet result = Query.getRS("SELECT DISTINCT Type FROM appointments");
@@ -168,6 +225,14 @@ public class ApptDaoImpl {
         }
         return allTypes;
     }
+
+    /**
+     * Gets the count of appointments of a certain type that falls within a certain month.
+     * @param type type of appointment
+     * @param month month of appointment
+     * @return count of appointments that match the type and month.
+     * @throws SQLException
+     */
     public static int getCount(String type, int month) throws SQLException {
         int count = 0;
         PreparedStatement ps = Query.getPS("SELECT COUNT(*) FROM appointments WHERE Type = ? AND MONTH(Start) = ?");
