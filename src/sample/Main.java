3aa37bd4b1a6d0c3ab8f5a98b9219ce130b167ca
unknown <pokeviv@gmail.com>
2022-09-03 10:14:55 -0400

    clean up Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,17 +15,6 @@ import java.util.ResourceBundle;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
-        /*Locale.setDefault(new Locale("fr"));
-        ResourceBundle rb = ResourceBundle.getBundle("sample/Nat", Locale.getDefault());
-        if(!Locale.getDefault().getLanguage().equals("fr")){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("This is NOT french");
-        }
-        if(Locale.getDefault().getLanguage().equals("fr")){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("This is in french");
-        }
-*/
         Parent root = FXMLLoader.load(getClass().getResource("/view/loginScreen.fxml"));
         stage.setTitle("Login");
         stage.setScene(new Scene(root, 600, 400));
@@ -34,28 +23,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        /*Locale.setDefault(new Locale("fr"));
-        ResourceBundle rb = ResourceBundle.getBundle("sample/Nat", Locale.getDefault());
-
-        if(!Locale.getDefault().getLanguage().equals("fr")){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("This is NOT french");
-        }
-        if(Locale.getDefault().getLanguage().equals("fr")){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("This is in french");
-        }*/
-        //ResourceBundle rb = ResourceBundle.getBundle("sample/Nat", new Locale("fr"));
-        /*
-        if(Locale.getDefault().getLanguage().equals("fr")){
-            french = true;
-            System.out.println(rb.getString("Submit"));
-        }
-*/
         DBConnection.openConnection();
-        //launch(args); pulls a bunch of methods that start loading your GUIs, so you want to connect to database
-        //before this launch method is called, and you want to close database after it is called
-
         launch(args);
         DBConnection.closeConnection();
     }
